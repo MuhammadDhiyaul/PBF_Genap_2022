f3ec6a8f20f5ae6b2580f7c607e3ce7f2cf6facd
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2022-06-06 10:20:01 +0700

    Update Konfigurasi Firestore

--- Minggu12/react-firestore/src/Firebase.js
+++ Minggu12/react-firestore/src/Firebase.js
@@ -3,16 +3,26 @@ import 'firebase/compat/firestore';
 
 const settings = {timestampsInSnapshots: true};
 
+// const config = {
+//   apiKey: "AIzaSyB1dW-X5MnIVbktbSeyeNinfvrsV0G8WYo",
+//   authDomain: "flutterfirebase-2be5d.firebaseapp.com",
+//   databaseURL: "https://flutterfirebase-2be5d-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "flutterfirebase-2be5d",
+//   storageBucket: "flutterfirebase-2be5d.appspot.com",
+//   messagingSenderId: "688280762161",
+//   appId: "1:688280762161:web:cceb1284e1cd0bfcb86210",
+//   measurementId: "G-C8CXTNMWFG"
+// };
+
 const config = {
-  apiKey: "AIzaSyB1dW-X5MnIVbktbSeyeNinfvrsV0G8WYo",
-  authDomain: "flutterfirebase-2be5d.firebaseapp.com",
-  databaseURL: "https://flutterfirebase-2be5d-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "flutterfirebase-2be5d",
-  storageBucket: "flutterfirebase-2be5d.appspot.com",
-  messagingSenderId: "688280762161",
-  appId: "1:688280762161:web:cceb1284e1cd0bfcb86210",
-  measurementId: "G-C8CXTNMWFG"
+  apiKey: "AIzaSyDz03V1ulFF86J7AVq6Jx3__xWSCZNaMBc",
+  authDomain: "minggu12-reactfirestore.firebaseapp.com",
+  projectId: "minggu12-reactfirestore",
+  storageBucket: "minggu12-reactfirestore.appspot.com",
+  messagingSenderId: "520371203503",
+  appId: "1:520371203503:web:56426ad5cf4092b72e135a"
 };
+
 firebase.initializeApp(config);
 firebase.firestore().settings(settings);
 
